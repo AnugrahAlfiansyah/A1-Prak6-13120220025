bf0b10a0b3b02f2eb52f155448bb8827ffce7e2f
aisananta <kolopuha415@gmail.com>
2024-05-18 17:42:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a1_prak6_13120220025/RestCallbackMahasiswa.java
@@ -0,0 +1,7 @@
+package com.example.a1_prak6_13120220025;
+
+import java.util.ArrayList;
+
+public interface RestCallbackMahasiswa {
+    void requestDataMhsSuccess(ArrayList<Mahasiswa> arrayList);
+}
